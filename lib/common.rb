240385edbda55de2c6dc8fbc0dbaaf157a02b567
weshays <weshays@gbdev.com>
2012-05-11 20:26:59 -0700

    Removed comments

--- lib/common.rb
+++ lib/common.rb
@@ -46,30 +46,3 @@ module TheCity
   end  
 
 end
-
-# {"X-City-Sig"=>"Zp4fxcR4QbImWtN11zesuq2TIsp3EfH%2FMrVdUlLf9aY%3D",
-#  "X-City-User-Token"=>"e43ab88416b3b5b7",
-#   "X-City-Time"=>"1336792053", 
-#   "Accept"=>"application/vnd.thecity.admin.v1+json"}
-
-# key = '6db4c76b5e8fb6ef09055eeaa4b977326c70c371'
-# token = 'e43ab88416b3b5b7' 
-# path = '/groups'
-# url = 'http://api.devthecity.org:9292' + path
-# current_time = Time.now.to_i.to_s
-# string_to_sign = current_time.to_s + 'GET' + url
-# unencoded_hmac = OpenSSL::HMAC.digest('sha256', key, string_to_sign)
-# unescaped_hmac = Base64.encode64(unencoded_hmac).chomp
-# hmac_signature = CGI.escape(unescaped_hmac)
-# http = Net::HTTP.new('api.devthecity.org', 9292)
-# #http.use_ssl = true
-# headers = {}
-# headers['X-City-Sig'] = hmac_signature
-# headers['X-City-User-Token'] = token
-# headers['X-City-Time'] = current_time
-# headers['Accept'] = 'application/vnd.thecity.admin.v1+json'
-# response = http.get(path, headers)
-# #puts response.body
-# puts response
-
-
