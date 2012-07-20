b16c19fb7d287a76bd76c58e5d8a4a631db7a24d
weshays <weshays@gbdev.com>
2012-07-20 11:21:45 -0700

    Stubbed spec files for API.

---
+++ spec/api/group_address_list_spec.rb
@@ -0,0 +1,21 @@
+require File.join(Dir.pwd, 'spec', 'spec_helper')
+
+describe 'GroupAddressList' do
+
+  before do
+    simulate_connection_to_server
+  end
+
+  after do
+
+  end
+
+
+  it 'should pass if ....' do
+    true.should == true
+  end
+
+
+end
+
+
---
+++ spec/api/group_role_list_spec.rb
@@ -0,0 +1,21 @@
+require File.join(Dir.pwd, 'spec', 'spec_helper')
+
+describe 'GroupRoleList' do
+
+  before do
+    simulate_connection_to_server
+  end
+
+  after do
+
+  end
+
+
+  it 'should pass if ....' do
+    true.should == true
+  end
+
+
+end
+
+
