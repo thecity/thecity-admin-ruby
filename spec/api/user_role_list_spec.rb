a75260c2335db48d9826c70924ed4ea1d0e53fd0
weshays <weshays@gbdev.com>
2012-07-24 13:57:14 -0700

    Added specs for user roles and skills

--- spec/api/user_role_list_spec.rb
+++ spec/api/user_role_list_spec.rb
@@ -11,11 +11,91 @@ describe 'UserRoleList' do
   end
 
 
-  it 'should pass if user process list page number is not specifed' 
-  it 'should pass if user process list is empty'
-  it 'should return a valid list of user processes'
-  it 'should iterate using *each* method'
-  it 'should iterate using *collect* method'
+  it 'should pass if user role list attribute is not specifed' do
+    user_id = 123
+    request_data = FactoryGirl.attributes_for(:user_role_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :roles => [FactoryGirl.attributes_for(:user_role)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+
+    reader = TheCity::UserRoleListReader.new(user_id)
+    role_list = TheCity::UserRoleList.new(reader)
+
+    role = role_list[0]
+    role.group_name.should == "The Group"
+  end
+
+
+  it 'should pass if user role list is empty' do
+    user_id = 123
+    page = 2
+    request_data = FactoryGirl.attributes_for(:user_role_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :roles => []
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+    reader = TheCity::UserRoleListReader.new(user_id, page)
+    role_list = TheCity::UserRoleList.new(reader)
+
+    role_list.empty?.should be_true
+  end
+
+
+  it 'should return a valid list of user roles' do 
+    user_id = 123
+    page = 2
+    request_data = FactoryGirl.attributes_for(:user_role_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :roles => [FactoryGirl.attributes_for(:user_role)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+
+    reader = TheCity::UserRoleListReader.new(user_id, page)
+    role_list = TheCity::UserRoleList.new(reader)
+
+    role = role_list[0]
+    role.group_name.should == "The Group"
+  end
+
+
+  it 'should iterate using *each* method' do
+    user_id = 123
+    request_data = FactoryGirl.attributes_for(:user_role_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :roles => [FactoryGirl.attributes_for(:user_role)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+
+    reader = TheCity::UserRoleListReader.new(user_id)
+    role_list = TheCity::UserRoleList.new(reader)
+
+    roles = []
+    role_list.each { |role| roles << role.group_name }
+    roles.should == ["The Group"]
+  end  
+
+
+  it 'should iterate using *collect* method' do
+    user_id = 123
+    request_data = FactoryGirl.attributes_for(:user_role_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :roles => [FactoryGirl.attributes_for(:user_role)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+
+    reader = TheCity::UserRoleListReader.new(user_id)
+    role_list = TheCity::UserRoleList.new(reader)
+
+    roles = role_list.collect { |role| role.group_name }
+    roles.should == ["The Group"]
+  end   
+
 
 end
 
--- spec/api/user_skill_list_spec.rb
+++ spec/api/user_skill_list_spec.rb
@@ -11,12 +11,90 @@ describe 'UserSkillList' do
   end
 
 
-  it 'should pass if user skill list page number is not specifed' 
-  it 'should pass if user skill list is empty'
-  it 'should return a valid list of user skills'
-  it 'should iterate using *each* method'
-  it 'should iterate using *collect* method'
+  it 'should pass if user skill list attribute is not specifed' do
+    user_id = 123
+    request_data = FactoryGirl.attributes_for(:user_skill_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :skills => [FactoryGirl.attributes_for(:user_skill)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
 
+    reader = TheCity::UserSkillListReader.new(user_id)
+    skill_list = TheCity::UserSkillList.new(reader)
+
+    skill = skill_list[0]
+    skill.name.should == "Teaching-Curriculum"
+  end
+
+
+  it 'should pass if user skill list is empty' do
+    user_id = 123
+    page = 2
+    request_data = FactoryGirl.attributes_for(:user_skill_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :skills => []
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+    reader = TheCity::UserSkillListReader.new(user_id, page)
+    skill_list = TheCity::UserSkillList.new(reader)
+
+    skill_list.empty?.should be_true
+  end
+
+
+  it 'should return a valid list of user skills' do 
+    user_id = 123
+    page = 2
+    request_data = FactoryGirl.attributes_for(:user_skill_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :skills => [FactoryGirl.attributes_for(:user_skill)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+
+    reader = TheCity::UserSkillListReader.new(user_id, page)
+    skill_list = TheCity::UserSkillList.new(reader)
+
+    skill = skill_list[0]
+    skill.name.should == "Teaching-Curriculum"
+  end
+
+
+  it 'should iterate using *each* method' do
+    user_id = 123
+    request_data = FactoryGirl.attributes_for(:user_skill_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :skills => [FactoryGirl.attributes_for(:user_skill)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+
+    reader = TheCity::UserSkillListReader.new(user_id)
+    skill_list = TheCity::UserSkillList.new(reader)
+
+    skills = []
+    skill_list.each { |skill| skills << skill.name }
+    skills.should == ["Teaching-Curriculum"]
+  end  
+
+
+  it 'should iterate using *collect* method' do
+    user_id = 123
+    request_data = FactoryGirl.attributes_for(:user_skill_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :skills => [FactoryGirl.attributes_for(:user_skill)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+
+    reader = TheCity::UserSkillListReader.new(user_id)
+    skill_list = TheCity::UserSkillList.new(reader)
+
+    skills = skill_list.collect { |skill| skill.name }
+    skills.should == ["Teaching-Curriculum"]
+  end   
 
 
 end
