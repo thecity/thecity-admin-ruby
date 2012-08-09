1fa38563d8d8278d827d39e1eb9a7d7f7102699c
weshays <weshays@gbdev.com>
2012-08-09 11:01:47 -0700

    Finished tests for checkin list.

--- spec/api/group_checkin_list_spec.rb
+++ spec/api/group_checkin_list_spec.rb
@@ -26,66 +26,66 @@ describe 'GroupCheckinList' do
   end
 
 
-  # it 'should pass if group checkin list is empty' do
-  #   group_id = 123
-  #   page = 2
-  #   request_data = FactoryGirl.attributes_for(:group_checkin_list, {
-  #     :total_entries => 1,
-  #     :total_pages => 1,
-  #     :checkins => []
-  #   }).to_json
-  #   TheCity.stub(:admin_request).and_return(request_data)
-  #   checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id})
+  it 'should pass if group checkin list is empty' do
+    group_id = 123
+    page = 2
+    request_data = FactoryGirl.attributes_for(:group_checkin_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :checkins => []
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+    checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id})
 
-  #   checkin_list.empty?.should be_true
-  # end
-
-
-  # it 'should return a valid list of group checkins' do 
-  #   group_id = 123
-  #   page = 2
-  #   request_data = FactoryGirl.attributes_for(:group_checkin_list, {
-  #     :total_entries => 1,
-  #     :total_pages => 1,
-  #     :checkins => [FactoryGirl.attributes_for(:group_checkin)]
-  #   }).to_json
-  #   TheCity.stub(:admin_request).and_return(request_data)
-  #   checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id, :page => 2})
-
-  #   checkin = checkin_list[0]
-  #   checkin.city.should == "Sammamish"
-  # end
+    checkin_list.empty?.should be_true
+  end
 
 
-  # it 'should iterate using *each* method' do
-  #   group_id = 123
-  #   request_data = FactoryGirl.attributes_for(:group_checkin_list, {
-  #     :total_entries => 1,
-  #     :total_pages => 1,
-  #     :checkins => [FactoryGirl.attributes_for(:group_checkin)]
-  #   }).to_json
-  #   TheCity.stub(:admin_request).and_return(request_data)
-  #   checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id})
+  it 'should return a valid list of group checkins' do 
+    group_id = 123
+    page = 2
+    request_data = FactoryGirl.attributes_for(:group_checkin_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :checkins => [FactoryGirl.attributes_for(:group_checkin)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+    checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id, :page => 2})
 
-  #   checkins = []
-  #   checkin_list.each { |checkin| checkins << checkin.city }
-  #   checkins.should == ["Sammamish"]
-  # end  
+    checkin = checkin_list[0]
+    checkin.parent_group_name.should == "Ballard Childrens ministry"
+  end
 
 
-  # it 'should iterate using *collect* method' do
-  #   group_id = 123
-  #   request_data = FactoryGirl.attributes_for(:group_checkin_list, {
-  #     :total_entries => 1,
-  #     :total_pages => 1,
-  #     :checkins => [FactoryGirl.attributes_for(:group_checkin)]
-  #   }).to_json
-  #   TheCity.stub(:admin_request).and_return(request_data)
-  #   checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id})
+  it 'should iterate using *each* method' do
+    group_id = 123
+    request_data = FactoryGirl.attributes_for(:group_checkin_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :checkins => [FactoryGirl.attributes_for(:group_checkin)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+    checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id})
 
-  #   checkins = checkin_list.collect { |checkin| checkin.city }
-  #   checkins.should == ["Sammamish"]
-  # end   
+    checkins = []
+    checkin_list.each { |checkin| checkins << checkin.parent_group_name }
+    checkins.should == ["Ballard Childrens ministry"]
+  end  
+
+
+  it 'should iterate using *collect* method' do
+    group_id = 123
+    request_data = FactoryGirl.attributes_for(:group_checkin_list, {
+      :total_entries => 1,
+      :total_pages => 1,
+      :checkins => [FactoryGirl.attributes_for(:group_checkin)]
+    }).to_json
+    TheCity.stub(:admin_request).and_return(request_data)
+    checkin_list = TheCity::GroupCheckinList.new({:group_id => group_id})
+
+    checkins = checkin_list.collect { |checkin| checkin.parent_group_name }
+    checkins.should == ["Ballard Childrens ministry"]
+  end   
 
 
 end
