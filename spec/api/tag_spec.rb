64b5e036fca6b0600086715b4a5c552b2c8329aa
weshays <weshays@gbdev.com>
2012-08-14 14:54:06 -0700

    Added spec

---
+++ spec/api/tag_spec.rb
@@ -0,0 +1,19 @@
+require File.join(Dir.pwd, 'spec', 'spec_helper')
+
+describe 'Tag' do
+
+  before do
+    simulate_connection_to_server
+  end
+
+  after do
+
+  end
+
+
+  it 'should pass if ....' 
+
+
+end
+
+
