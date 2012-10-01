c51f348d53300cfc54d0a23a3eb5c08841f84864
weshays <weshays@gbdev.com>
2012-09-30 20:55:41 -0700

    Update thecity_headers.rb example file.

--- thecity_headers.rb
+++ thecity_headers.rb
@@ -3,25 +3,47 @@ require 'CGI'
 require 'openssl'
 require 'net/http'
 require 'net/https'
+require 'json'
 
-key = '6db4c76b5e8fb6ef09055eeaa4b977326c70c371'
-token = 'e43ab88416b3b5b7' 
+
+# The City API keys
+key = '**** YOUR CITY API KEY ****'
+token = '**** YOUR CITY API TOKEN ****'
+
+
+# API endpoint to get data from
 path = '/groups'
-url = 'http://api.devthecity.org:9292' + path
+url = 'https://api.onthecity.org' + path
+
+
+# Build signature
 current_time = Time.now.to_i.to_s
 string_to_sign = current_time.to_s + 'GET' + url
 unencoded_hmac = OpenSSL::HMAC.digest('sha256', key, string_to_sign)
 unescaped_hmac = Base64.encode64(unencoded_hmac).chomp
 hmac_signature = CGI.escape(unescaped_hmac)
-http = Net::HTTP.new('api.devthecity.org', 9292)
-#http.use_ssl = true
+
+
+# Build headers
 headers = {}
 headers['X-City-Sig'] = hmac_signature
 headers['X-City-User-Token'] = token
 headers['X-City-Time'] = current_time
 headers['Accept'] = 'application/vnd.thecity.admin.v1+json'
-response = http.get(path, headers)
-#puts response.body
-puts response
+headers['Content-Type'] = 'application/json'
 
 
+# API Request
+uri = URI.parse(url)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+request = Net::HTTP::Get.new(uri.request_uri, headers)
+response = http.request(request)
+groups_data = JSON.parse(response.body)
+
+
+# Output group names
+groups_data['groups'].each do |group| 
+  puts group['name']
+end
