b4e5af26f99fc026f4760c4bbe669ffcf06c7ea4
weshays <weshays@gbdev.com>
2013-01-15 20:08:52 -0800

    comment change

--- lib/readers/group_export_reader.rb
+++ lib/readers/group_export_reader.rb
@@ -10,7 +10,7 @@ module TheCity
     # @param cacher (optional) The CacheAdapter cacher to be used to cache data.
     def initialize(group_export_id, options = {}, cacher = nil)
       group_id = options[:group_id]
-      #@class_key = "group_export_#{group_id}"   
+      #@class_key = "group_export_#{group_id}_#{group_export_id}"   
       @url_data_path = "/groups/#{group_id}/exports/#{group_export_id}"
       @url_data_params = options
       
