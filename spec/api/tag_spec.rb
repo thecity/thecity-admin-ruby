bb3a8c48af3f8cb0262e8367fddd1341bbbbf27a
markjflores <flores.markj@gmail.com>
2013-06-04 15:29:15 -0700

    Checkpoint.  Got .attributes_for (hash method) working, but unable to run valid tests.  tag.should be_valid returns errors.

--- spec/api/tag_spec.rb
+++ spec/api/tag_spec.rb
@@ -2,6 +2,8 @@ require File.join(Dir.pwd, 'spec', 'spec_helper')
 
 describe 'Tag' do
 
+  tag = FactoryGirl.attributes_for(:tag)
+
   before do
     simulate_connection_to_server
   end
@@ -11,8 +13,17 @@ describe 'Tag' do
   end
 
 
-  it 'should pass if ....' 
-
+  context 'with valid params' do
+    it 'will save' do
+      tag[:name].should be_type_of(String)
+      tag[:id].should be_type_of(Integer)
+      tag[:created_at].should be_type_of(Date)
+    end
+  end
+  
+  context 'with invalid params' do
+    
+  end
 
 end
 
